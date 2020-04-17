08f18d5cf60a73ba1fc28a18c99743665c11d5d5
Kenneth Siu <kenneth.siu@softwire.com>
2020-04-17 15:49:15 +0100

    Calculator introduces itself

---
+++ src/training/calculatorreview/Main.java
@@ -0,0 +1,12 @@
+package training.calculatorreview;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the calculator!");
+        System.out.println("==========================");
+        new Scanner(System.in);
+    }
+}
